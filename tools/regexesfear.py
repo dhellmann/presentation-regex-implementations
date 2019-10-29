a778d81c59e75654827e70ae93197e7d2edc6746
Doug Hellmann <doug@doughellmann.com>
2019-10-29 07:39:46 -0400

    start intro
    add code from previous presentation to show the output of a regex match
    
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

---
+++ tools/regexesfear.py
@@ -0,0 +1,59 @@
+import re
+
+from tools.output import capture
+
+HIGHLIGHT = (
+    '<span class="fragment highlight-yellow" '
+    'data-fragment-index="1">'
+    '{}'
+    '</span>'
+)
+
+
+@capture
+def showregex(s, p, flags=0, heading='h3', classes='',
+              pre_style='font-size: 100%'):
+    if flags & re.VERBOSE:
+        print('\n<pre data-trim data-noescape style="font-size: 80%">')
+        print(p.replace('<', '&lt;'))
+        print('</pre>')
+    else:
+        print('<{heading} class="code {classes}">{p}</{heading}>'.format(
+            p=p,
+            heading=heading,
+            classes=classes,
+        ))
+
+    print('\n<pre data-trim data-noescape style="{}">'.format(pre_style))
+
+    pat = re.compile(p, flags)
+
+    if not pat.search(s):
+        print(s)
+        print('<span class="fragment fade-in">(no match)</span>')
+    else:
+        prev = 0
+        last = 0
+        for m in pat.finditer(s):
+            if prev < m.start():
+                print(s[prev:m.start()], end='')
+            prev = m.end()
+            print(HIGHLIGHT.format(s[m.start():m.end()]), end='')
+            last = m.end()
+        if last:
+            print(s[last:], end='')
+        print()
+
+        for i, m in enumerate(pat.finditer(s), 1):
+            if m.groupdict():
+                print('<span class="fragment fade-in" data-fragment-index="1">')
+                for n, v in sorted(m.groupdict().items()):
+                    print('{:<10s}: {}'.format(n, v))
+                print('</span>')
+            elif m.groups():
+                print('<span class="fragment fade-in" data-fragment-index="1">')
+                for j, g in enumerate(m.groups(), 1):
+                    print('({}, {}) {}'.format(i, j, g))
+                print('</span>')
+
+    print('</pre>')
