7cde315594cc1aa055a5fbf8fc42ce720caa85d9
Doug Hellmann <doug@doughellmann.com>
2019-11-02 12:04:15 -0400

    pike: restore minimal print statements
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- code/pike.py
+++ code/pike.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
+# Based on the code in
+# https://www.cs.princeton.edu/courses/archive/spr09/cos333/beautiful.html
+# by Rob Pike.
+
 
 def match(regexp, text):
     if regexp and regexp[0] == '^':
         return match_here(regexp[1:], text)
     while text:
+        print('\nmatch({!r}, {!r})'.format(regexp, text))
         if match_here(regexp, text):
             return True
         text = text[1:]
@@ -12,19 +17,21 @@ def match(regexp, text):
 
 
 def match_here(regexp, text):
+    print('match_here({!r}, {!r})'.format(regexp, text))
     if not regexp:
         return True
     if len(regexp) > 1 and regexp[1] == '*':
         return match_star(regexp[0], regexp[2:], text)
     if len(regexp) == 1 and regexp[0] == '$':
         return len(text) == 0
-    if text and regexp and (regexp[0] == '.' or regexp[0] == text[0]):
+    if text and regexp and (regexp[0] in ['.', text[0]]):
         return match_here(regexp[1:], text[1:])
     return False
 
 
 def match_star(c, regexp, text):
     while True:
+        print('match_star({!r}, {!r}, {!r})'.format(c, regexp, text))
         if match_here(regexp, text):
             return True
         if c not in ['.', text[0]]:
